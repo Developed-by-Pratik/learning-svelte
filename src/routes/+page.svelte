54e9c47bd71f19259064f2320317a65b89fbe48f
Pratik <Pratikkakade4618@gmail.com>
2026-02-08 10:50:53 +0530

    Derived State Example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
     import Header from "./Header.svelte";
     import Toggler from "./ToggleStatus.svelte"
+    
     let name = "Pratik";
     let nameState = $state('John');
+
+    let derivedName = $derived(name + " " + "Derived Name");
+
 </script>
 
 <!-- <Header name="Kartik" />
@@ -10,7 +14,12 @@
 <Header name = {nameState} />
 <input type="text" bind:value={nameState}> -->
 
-<Header /> // This is throwing an error because we havent passed name prop
-// So to make the prop optional we can give deafult value in child component
+<!-- <Header /> // This is throwing an error because we havent passed name prop
+// So to make the prop optional we can give deafult value in child component -->
+
+
+<Header name = {derivedName}/>
+<input type="text" bind:value={derivedName}>
 
 <!-- <Toggler /> -->
+
